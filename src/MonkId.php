e6b9236963b44cb854a5d044a95a4b29ab9ce636
Justin Stayton <justin.stayton@gmail.com>
2013-08-20 09:45:28 -0500

    Initial commit of rewrite that works with popup authentication payloads.

--- src/MonkId.php
+++ src/MonkId.php
@@ -1,304 +1,112 @@
 <?php
 
-  /**
-   * PHP client for Monk ID.
-   *
-   * @author    Justin Stayton <jstayton@monkdevelopment.com>
-   * @copyright Copyright 2012 by Monk Development, Inc.
-   * @license   http://en.wikipedia.org/wiki/MIT_License MIT License
-   * @package   MonkId
-   * @version   0.2.0
-   */
-  class MonkId {
+  namespace Monk;
 
-    /**
-     * Authentication token field of `user` object in API requests/responses.
-     */
-    const AUTH_TOKEN = 'authentication_token';
+  class Id {
 
-    /**
-     * Confirmed date/time field of `user` object in API requests/responses.
-     */
-    const CONFIRMED_AT = 'confirmed_at';
+    private static $instance;
 
-    /**
-     * Email field of `user` object in API requests/responses.
-     */
-    const EMAIL = 'email';
+    private $config = array();
+    private $payload;
 
-    /**
-     * Last log in date/time field of `user` object in API requests/responses.
-     */
-    const LAST_LOG_IN_AT = 'last_sign_in_at';
+    private function __construct() { }
 
-    /**
-     * Logged in date/time field of `user` object in API requests/responses.
-     */
-    const LOGGED_IN = 'signed_in';
+    final private function __clone() { }
 
-    /**
-     * Logged out date/time field of `user` object in API requests/responses.
-     */
-    const LOGGED_OUT_AT = 'logged_out_at';
+    public static function getInstance() {
+      if (!isset(self::$instance)) {
+        self::$instance = new self();
+      }
 
-    /**
-     * One time token field of `user` object in API requests/responses.
-     */
-    const ONE_TIME_TOKEN = 'one_time_token';
+      return self::$instance;
+    }
 
-    /**
-     * Password field of `user` object in API requests/responses.
-     */
-    const PASSWORD = 'password';
-
-    /**
-     * An unknown error occurred.
-     */
-    const STATUS_UNKNOWN_ERROR = -1;
-
-    /**
-     * Request successful.
-     */
-    const STATUS_SUCCESS = 0;
-
-    /**
-     * Invalid email address.
-     */
-    const STATUS_INVALID_EMAIL = 1;
-
-    /**
-     * Email address already exists.
-     */
-    const STATUS_EMAIL_EXISTS = 2;
-
-    /**
-     * Invalid password.
-     */
-    const STATUS_INVALID_PASSWORD = 3;
-
-    /**
-     * Invalid password, cannot be less than six characters.
-     */
-    const STATUS_INVALID_PASSWORD_SHORT = 4;
-
-    /**
-     * Invalid password, cannot be blank.
-     */
-    const STATUS_INVALID_PASSWORD_BLANK = 5;
-
-    /**
-     * DELETE method for HTTP requests.
-     */
-    const HTTP_DELETE = 'DELETE';
-
-    /**
-     * GET method for HTTP requests.
-     */
-    const HTTP_GET = 'GET';
-
-    /**
-     * POST method for HTTP requests.
-     */
-    const HTTP_POST = 'POST';
-
-    /**
-     * PUT method for HTTP requests.
-     */
-    const HTTP_PUT = 'PUT';
-
-    /**
-     * Config to use across all requests.
-     *
-     * @var array
-     */
-    private static $config = array();
-
-    /**
-     * Load an ini config from a file to use across all requests.
-     *
-     * @link   http://php.net/manual/en/function.parse-ini-file.php
-     * @link   http://en.wikipedia.org/wiki/INI_file
-     * @param  string $path path to the config file to load
-     * @param  string $environment environment name within the config to load
-     * @return bool true if loaded, false if failed to load or no config
-     */
-    public static function loadConfig($path, $environment) {
+    public function loadConfig($path, $environment) {
       $config = parse_ini_file($path, true);
 
-      if (!$config || !array_key_exists($environment, $config)) {
+      if (!$config || !isset($config[$environment])) {
         return false;
       }
 
-      self::$config = $config[$environment];
+      $this->config = $config[$environment];
 
       return true;
     }
 
-    /**
-     * Set a config value to use across all requests.
-     *
-     * @param  string $key key to set
-     * @param  mixed $value value to set
-     * @return MonkId this class
-     */
-    public static function setConfig($key, $value) {
-      self::$config[$key] = $value;
+    public function setConfig($key, $value) {
+      $this->config[$key] = $value;
 
-      return self;
+      return $this;
     }
 
-    /**
-     * Get a config value that's used across all requests.
-     * 
-     * @param  string $key key value to get
-     * @return mixed|null key value or null if not set
-     */
-    public static function getConfig($key) {
-      return array_key_exists($key, self::$config) ? self::$config[$key] : null;
+    public function getConfig($key) {
+      return isset($this->config[$key]) ? $this->config[$key] : null;
     }
 
-    /**
-     * Register a new user.
-     * 
-     * @param  string $email email address of user
-     * @param  string $password password of user
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    public static function register($email, $password) {
-      $params = array(self::EMAIL    => $email,
-                      self::PASSWORD => $password);
-
-      return self::request(self::HTTP_POST, '/', $params);
+    private function cookiePayload() {
+      return isset($_COOKIE['_monkIdPayload']) ? $_COOKIE['_monkIdPayload'] : null;
     }
 
-    /**
-     * Update a user's username and/or password.
-     * 
-     * @param  string $email new email address of user
-     * @param  string $password new password of user
-     * @param  string $authToken authentication token for user
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    public static function update($email, $password, $authToken) {
-      $params = array(self::AUTH_TOKEN => $authToken,
-                      self::EMAIL      => $email,
-                      self::PASSWORD   => $password);
-
-      return self::request(self::HTTP_PUT, '/', $params);
+    private function decodePayload($payload) {
+      return json_decode(base64_decode($payload), true);
     }
 
-    /**
-     * Send a password reset email to a user.
-     * 
-     * @param  string $email email address of user
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    public static function passwordReset($email) {
-      $params = array(self::EMAIL => $email);
-
-      return self::request(self::HTTP_POST, '/password', $params);
-    }
-
-    /**
-     * Log a user in to Monk ID.
-     * 
-     * @param  string $email email address of user
-     * @param  string $password password of user
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    public static function logIn($email, $password) {
-      $params = array(self::EMAIL    => $email,
-                      self::PASSWORD => $password);
-
-      return self::request(self::HTTP_POST, '/sign_in', $params);
-    }
-
-    /**
-     * Log a user out of Monk ID.
-     * 
-     * @param  string $authToken authentication token for user
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    public static function logOut($authToken) {
-      $params = array(self::AUTH_TOKEN => $authToken);
-
-      return self::request(self::HTTP_DELETE, '/sign_out', $params);
-    }
-
-    /**
-     * Get the log in status of a user.
-     * 
-     * @param  string $authToken authentication token for user
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    public static function status($authToken) {
-      $params = array(self::AUTH_TOKEN => $authToken);
-
-      return self::request(self::HTTP_POST, '/status', $params);
-    }
-
-    /**
-     * Build the full request URL, including protocol, host, port, and path.
-     * 
-     * @param  string $endpoint endpoint of the URL
-     * @return string|false full request URL or false if host is not configured
-     */
-    private static function url($endpoint) {
-      $host = self::getConfig('host');
-
-      if (!$host) {
+    private function verifyPayload($payload) {
+      if (!$payload) {
         return false;
       }
 
-      $protocol = self::getConfig('ssl') ? 'https' : 'http';
-      $port = self::getConfig('port');
-      $path = '/api/users';
+      $signature = base64_decode($payload['user']['signature']);
 
-      return $protocol . '://' . $host . ($port ? ':' . $port : '') . $path . $endpoint;
+      unset($payload['user']['signature']);
+
+      $expectedSignature = hash_hmac('sha512', json_encode($payload['user']), $this->getConfig('appSecret'), true);
+
+      return $signature == $expectedSignature;
     }
 
-    /**
-     * Build the parameters to send with the request.
-     * 
-     * @param  array $user `user` key values
-     * @param  bool $queryString optional whether to encode to query string
-     * @return array|string string if encoded to query string
-     */
-    private static function params(array $user, $queryString = true) {
-      $params = array('api_key' => self::getConfig('api_key'),
-                      'user'    => $user);
+    public function loadPayload($encodedPayload = null) {
+      $payload = $encodedPayload ? $encodedPayload : $this->cookiePayload();
 
-      return $queryString ? http_build_query($params) : $params;
-    }
+      if (!$payload) {
+        $this->payload = null;
 
-    /**
-     * Make an HTTP API request using cURL.
-     * 
-     * @param  string $method HTTP method (GET, POST, etc.)
-     * @param  string $endpoint endpoint of the URL
-     * @param  array $params query string params to encode with the request
-     * @return array|false JSON-decoded response or false if request failed
-     */
-    private static function request($method, $endpoint, array $params) {
-      $url = self::url($endpoint);
-
-      if (!$url) {
         return false;
       }
 
-      $request = curl_init();
+      $payload = $this->decodePayload($payload);
+      $verified = $this->verifyPayload($payload);
 
-      curl_setopt($request, CURLOPT_CUSTOMREQUEST, $method);
-      curl_setopt($request, CURLOPT_POSTFIELDS, self::params($params));
-      curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($request, CURLOPT_URL, $url);
+      $this->payload = $verified ? $payload : null;
 
-      $response = curl_exec($request);
+      return $verified;
+    }
 
-      curl_close($request);
+    private function payload() {
+      if (!isset($this->payload)) {
+        $this->loadPayload();
+      }
 
-      return $response ? json_decode($response, true) : $response;
+      return $this->payload;
+    }
+
+    private function payloadValue($key) {
+      $payload = $this->payload();
+
+      if (isset($payload['user'][$key])) {
+        return $payload['user'][$key];
+      }
+      else {
+        return null;
+      }
+    }
+
+    public function userId() {
+      return $this->payloadValue('id');
+    }
+
+    public function userEmail() {
+      return $this->payloadValue('email');
     }
 
   }
