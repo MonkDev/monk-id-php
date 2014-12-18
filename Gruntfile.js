78b11cd31a82c2f5c807b2583868759872a168e6
Justin Stayton <justin.stayton@gmail.com>
2014-12-18 14:10:04 -0500

    Added two dev dependencies that make Grunt better.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
+'use strict';
+
 module.exports = function (grunt) {
-  'use strict';
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
 
   var optionIncrement = grunt.option('increment');
 
@@ -180,16 +183,4 @@ module.exports = function (grunt) {
   grunt.registerTask('quality', ['phplint', 'phpcs', 'phpcpd', 'phploc', 'phpdcd', 'phpmd', 'security-checker']);
   grunt.registerTask('build', ['jshint', 'test', 'quality', 'phpdoc']);
   grunt.registerTask('deploy', ['prompt:deploy', 'bump-increment', 'build', 'bump::commit-only', 'gh-pages']);
-
-  grunt.loadNpmTasks('grunt-bump');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-gh-pages');
-  grunt.loadNpmTasks('grunt-phpcpd');
-  grunt.loadNpmTasks('grunt-phpcs');
-  grunt.loadNpmTasks('grunt-phplint');
-  grunt.loadNpmTasks('grunt-phpmd');
-  grunt.loadNpmTasks('grunt-phpunit');
-  grunt.loadNpmTasks('grunt-prompt');
-  grunt.loadNpmTasks('grunt-shell');
 };
